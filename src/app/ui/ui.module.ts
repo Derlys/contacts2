030e7bf4879b33033151dc75d8571a1375a847a6
Derlys Dominguez <derlys275@gmail.com>
2020-12-24 18:59:33 -0500

    refactor: ui changes

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
-import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [LayoutComponent, FooterComponent, HeaderComponent],
+  declarations: [LayoutComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
 })
 export class UiModule {}
