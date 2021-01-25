e6f8d9bef5b59e9e3f4c0af7ded2cca68f25aeab
Derlys Dominguez <derlys275@gmail.com>
2021-01-25 16:41:29 -0500

    feat: add view detail

--- src/app/pages/contact-detail/contact-detail.component.ts
+++ src/app/pages/contact-detail/contact-detail.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../data/contact';
+import { Observable } from 'rxjs';
+import { ContactService } from '../../data/services/contact.service';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrls: ['./contact-detail.component.scss']
+  styleUrls: ['./contact-detail.component.scss'],
 })
 export class ContactDetailComponent implements OnInit {
+  contact$: Observable<Contact | undefined> = this.route.params.pipe(
+    map((params) => params.contactId),
+    switchMap((id) => this.service.loadContact(id))
+  );
 
-  constructor() { }
+  constructor(public route: ActivatedRoute, public service: ContactService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
